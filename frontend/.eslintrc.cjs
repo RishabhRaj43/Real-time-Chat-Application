95495939ec8ee59af70bf7df97c2847e3b6312c1
RishabhRaj14 <rishabhraj4675@gmail.com>
2024-07-15 20:50:50 +0530

    Doing Deployment

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
+  extends: [ 
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
